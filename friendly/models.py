74fefb89310c91bc0fe36b28c28c512f205062d1
jacksonherron <jacksonherron@gmail.com>
2019-08-08 13:46:25 -0700

    Adds match and message models

--- friendly/models.py
+++ friendly/models.py
@@ -9,6 +9,7 @@ class Profile(models.Model):
     date_of_birth = models.DateField(auto_now=False, auto_now_add=False)
     gender_identity = models.CharField(max_length=100)
     languages = ArrayField(models.CharField(max_length=100))
+    zip_code = models.PositiveIntegerField(max_length=5)
     personal_description = models.TextField(null=True, blank=True)
     identities = ArrayField(models.CharField(max_length=100, null=True, blank=True), size=5, null=True, blank=True)
     interests = ArrayField(models.CharField(max_length=100, null=True, blank=True), size=10, null=True, blank=True)
@@ -17,3 +18,21 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Match(models.Model):
+    user_1 = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='primary_matches')
+    user_2 = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='secondary_matches')
+    validate = models.BooleanField(default=False)
+    time_stamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user_1.user.username} and {self.user_2.user.username}'
+
+class Message(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='messages')
+    match = models.ForeignKey(Match, on_delete=models.CASCADE, related_name='messages')
+    time_stamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.author.user.username}, {self.match}, {self.time_stamp}'
