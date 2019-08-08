1df0596ffc55884f12ca3c00fd6d33dabe01e64c
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2019-08-08 13:55:12 -0700

    adds crappy styling

--- friendly/urls.py
+++ friendly/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
     path('home/', views.home, name='home'),
+    path('profile/', views.profile, name='profile'),
+    path('match_list/', views.match_list, name='match_list'),
 ]
